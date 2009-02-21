ca228364ea3a1b6dce04ffc00ae3444a542f88ad
Max Aller <max@max-vm-laptop.(none)>
2009-02-21 13:20:56 -0800

    Almost done, still working out the kinks

--- lib/threadz.rb
+++ lib/threadz.rb
@@ -10,16 +10,13 @@
 # License::      Distributed under the MIT License
 require 'thread'
 
-=begin
-  T = ThreadPool.new
-  b = T.new_batch
-  b << lambda { puts "foo" },
-  b << lambda { puts "bar" },
-  b << [ lambda { puts "can" }, lamba { puts "monkey" }]
-  b.wait_until_done
-  if b.completed?
-
-=end
+# Example:
+#  T = ThreadPool.new
+#  b = T.new_batch
+#  b << lambda { puts "foo" },
+#  b << lambda { puts "bar" },
+#  b << [ lambda { puts "can" }, lamba { puts "monkey" }]
+#  b.wait_until_done
 
 # All logic is contained in the Threadz namespace
 module Threadz
@@ -40,8 +37,8 @@ module Threadz
     #                 By default, this is 4.
     # :maximum_size:: The highest number of threads that can be allocated.  By default, this is the minimum size x 5.
     # :kill_threshold:: Constant that determines when new threads are needed or when threads can be killed off.
-    #                   If the kill score falls to negative kill_threshold, then a thread is killed off and the
-    #                   kill score is reset.  If the kill score rises to positive kill_threshold, then a new thread
+    #                   If the internally tracked kill score falls to positive kill_threshold, then a thread is killed off and the
+    #                   kill score is reset.  If the kill score rises to negative kill_threshold, then a new thread
     #                   is created and the kill score is reset.  Every 0.01 seconds, the state of all threads in the
     #                   pool is checked.  If there is more than one idle thread (and we're above minimum size), the
     #                   kill score is incremented by THREADS_IDLE_SCORE.  If there are no idle threads (and
@@ -49,9 +46,9 @@ module Threadz
     def initialize(opts={})
       @min_size = opts[:initial_size] || 4 # documented
       @max_size = opts[:maximum_size] || @min_size * 5 # documented
-      @queue = {}
-      @batch_order = Queue.new
-      @working_count = {}
+
+      # This is our main queue for jobs
+      @queue = Queue.new
       @threads = []
       @min_size.times { spawn_thread }
       @killscore = 0
@@ -60,45 +57,31 @@ module Threadz
       spawn_watch_thread
     end
 
-
-    def process(batch_name=:default, &block)
-      (@queue[batch_name] ||= []).push block
-      @batch_order << batch_name
+    # Push a process onto the job queue for the thread pool to pick up.
+    # Note that using this method, you can't keep track of when the job
+    # finishes.  If you care about when it finishes, use batches.
+    def process(&block)
+      @queue << block
+      nil
     end
 
-#    def wait_until_done(batch_name=:default)
-#      return if @queue[batch_name].nil? or @working_count[batch_name].nil?
-#      sleep 0.001 until @queue[batch_name].empty?
-#      sleep 0.001 until @working_count[batch_name] <= 0
-#    end
-
+    # Return a new batch that's attached into this thread pool.  See Threadz::ThreadPool::Batch
+    # for documention on opts.
     def new_batch(opts={})
       return Batch.new(self, opts)
     end
 
     private
-    def jobs_queued
-      @batch_order.length
-    end
 
+    # Spin up a new thread
     def spawn_thread
       @threads << Thread.new do
         while true
-          next_batch = @batch_order.pop
-          x = nil
-          Thread.exclusive do
-            x = @queue[next_batch].pop
-            @working_count[next_batch] ||= 0
-            @working_count[next_batch] += 1
-          end
+          x = @queue.shift
           begin
             x.call
           rescue Exception => e
             puts e.inspect
-          ensure
-            #          Thread.exclusive do
-            @working_count[next_batch] -= 1
-            #          end
           end
           exit if Thread.current[:suicide]
         end
@@ -106,6 +89,7 @@ module Threadz
       puts "spawning thread: now thread count is #{@threads.length}" if $DEBUG
     end
 
+    # Kill a thread after it completes its current job
     def kill_thread
       Thread.exclusive do
         t = @threads.pop
@@ -120,10 +104,10 @@ module Threadz
       @watch_thread = Thread.new do
         while true
           # If there are idle threads and we're above minimum
-          if @batch_order.num_waiting > 1 && @threads.length > @min_size # documented
+          if @queue.num_waiting > 1 && @threads.length > @min_size # documented
             @killscore += THREADS_IDLE_SCORE
             # If there are no threads idle and we have room for more
-          elsif(@batch_order.num_waiting == 0 && @threads.length < @max_size) # documented
+          elsif(@queue.num_waiting == 0 && @threads.length < @max_size) # documented
             @killscore -= THREADS_KILL_SCORE
           else
             # Decay,
@@ -137,24 +121,25 @@ module Threadz
             @killscore > 0 ? kill_thread : spawn_thread
             @killscore = 0
           end
-#          if @killscore >= @killthreshold
-#            @killscore = 0
-#            kill_thread
-#          end
-#          if @killscore *-1 >= @killthreshold
-#            @killscore = 0
-#            spawn_thread
-#          end
+          
           sleep 0.01
         end
       end
     end
 
+    # A batch is a collection of jobs you care about that gets pushed off to
+    # the attached thread pool.  The calling thread can be signaled when the
+    # batch has completed executing.
     class Batch
+      # Creates a new batch attached to the given threadpool.  A number of options
+      # are available:
+      # +:latent+:: If latent, none of the jobs in the batch will actually start
+      #            executing until the +start+ method is called.
       def initialize(threadpool, opts={})
         @threadpool = threadpool
         @waiting_threads = []
         @jobs = 0
+        @when_done_blocks = []
 
         ## Options
 
@@ -163,52 +148,82 @@ module Threadz
         @job_queue = [] if @latent
       end
 
-      def << job
+      # Add a new job to the batch.  If this is a latent batch, the job can't
+      # be scheduled until the batch is #start'ed; otherwise it may start
+      # immediately.  The job can be anything that responds to +call+ or an
+      # array of objects that respond to +call+.
+      def <<(job)
         if job.is_a? Array
           job.each {|j| self << j}
-        else
+        elsif job.respond_to? :call
           @jobs += 1
           if @latent
             @job_queue << job
           else
             send_to_threadpool job
           end
+        else
+          raise "Not a valid job: needs to support #call"
         end
       end
 
       alias_method(:push, :<<)
 
+      # Put the current thread to sleep until the batch is done processing.
+      # There are options available:
+      # +:timeout+:: If specified, will only wait for at least this many seconds
+      #              for the batch to finish.  Typically used with #completed?
       def wait_until_done(opts={})
-        return if completed?
-        @waiting_threads << Thread.current
-        timeout = opts.key?(:timeout) ? opts[:timeout] : -1
-        if timeout > 0.0
-          sleep(timeout)
-        else
-          Thread.stop
+        Thread.exclusive do
+          return if completed?
+          @waiting_threads << Thread.current
+          timeout = opts.key?(:timeout) ? opts[:timeout] : -1
+          if timeout > 0.0
+            # Go to sleep for at most timeout seconds.  It will wake up again if
+            # Thread#wakeup is called on it, though.
+            sleep(timeout)
+          else
+            Thread.stop
+          end
         end
       end
 
+      # Returns true iff there are no unfinished jobs in the queue.
       def completed?
         return @jobs == 0
       end
 
+      # If this is a latent batch, start processing all of the jobs in the queue.
       def start
-        if @latent
-          until @job_queue.empty?
-            send_to_threadpool @job_queue.pop
+        
+        Thread.exclusive do # in case another thread tries to push new jobs onto the queue while we're starting
+          if @latent
+            until @job_queue.empty?
+              send_to_threadpool @job_queue.pop
+            end
+            return true
+          else
+            return false
           end
-          return true
-        else
-          return false
+        end
+      end
+
+      # Execute a given block when the batch has finished processing.  If the batch
+      # has already finished executing, execute immediately.
+      def when_done(&block)
+        Thread.exclusive do # in case a new job gets added between completed? and block.call
+          if completed?
+            block.call
+          else
+            @when_done_blocks << block
+          end
         end
       end
 
       private
       def signal_done
-        Thread.exclusive do
-          @waiting_threads.pop.wakeup until @waiting_threads.empty?
-        end
+        @waiting_threads.shift.wakeup until @waiting_threads.empty?
+        @when_done_blocks.shift.call until @when_done_blocks.empty?
       end
 
       def send_to_threadpool job
--- spec/threadz_spec.rb
+++ spec/threadz_spec.rb
@@ -7,58 +7,198 @@ describe Threadz do
       @T = Threadz::ThreadPool.new
     end
 
-    it "should support batches" do
+    it "should support process" do
       i = 0
-      b = @T.new_batch
-      b << lambda { i += 1 }
-      b << lambda { i -= 1 }
-      b << [lambda { i += 2}, lambda { i -= 1}]
-      b.wait_until_done
-      i.should == 1
-    end
-
-    it "should support latent batches" do
-      i = 0
-      b = @T.new_batch :latent => true
-      b << lambda { i += 1 }
-      b << lambda { i -= 1 }
-      b << [lambda { i += 2}, lambda { i -= 1}]
-      i.should == 0
+      3.times do
+        @T.process { i += 1}
+      end
       sleep 0.1
-      i.should == 0
-      b.start
-      b.wait_until_done
-      i.should == 1
+      
+      i.should == 3
     end
 
-    it "should support batches and waiting with timeouts" do
+    it "should support creating batches" do
       i = 0
-      b = @T.new_batch
-      b << lambda { i += 1 }
-      b << lambda { i -= 1 }
-      b << [lambda { i += 2}, lambda { 50000000.times {}}]
 
-      t = Time.now
-      timeout = 0.5
-
-      b.wait_until_done :timeout => timeout
-
-      b.completed?.should be_false
-      (Time.now - t).should >= 0.5
+      lambda { @T.new_batch }.should_not raise_error
+      lambda { @T.new_batch :latent => true }.should_not raise_error
     end
 
-    it "should support completed even without timeouts" do
-      i = 0
-      b = @T.new_batch
-      b << lambda { i += 1 }
-      b << lambda { i -= 1 }
-      b << [lambda { i += 2}, lambda { sleep 0.05 while i < 10 }]
-      b.completed?.should be_false
-      sleep 0.2
-      b.completed?.should be_false
-      i = 10
-      sleep 0.2
-      b.completed?.should be_true
+    describe Threadz::ThreadPool::Batch do
+      it "should support jobs" do
+        i = 0
+        b = @T.new_batch
+        10.times do
+          b << lambda { i += 1 }
+        end
+        b.wait_until_done
+
+        i.should == 10
+      end
+
+      it "should support arrays of jobs" do
+        i = 0
+        b = @T.new_batch
+        b << [lambda { i += 2}, lambda { i -= 1}]
+        b << [lambda { i += 2}]
+        b.wait_until_done
+
+        i.should == 3
+      end
+
+      it "should support reuse" do
+        i = 0
+        b = @T.new_batch
+        b << [lambda { i += 2}, lambda { i -= 1}, lambda { i -= 2 }]
+        b.wait_until_done
+        
+        i.should == -1
+
+        b << [lambda { i += 9}, lambda { i -= 3}, lambda { i -= 4 }]
+        b.wait_until_done
+
+        i.should == 1
+      end
+
+      it "should play nicely with instance variables" do
+        @i = 0
+        b = @T.new_batch
+        b << [lambda { @i += 2}, lambda { @i -= 1}]
+        b << lambda { @i += 2}
+        b.wait_until_done
+
+        @i.should == 3
+      end
+
+      it "should support latent option correctly" do
+        i = 0
+        b = @T.new_batch(:latent => true)
+        b << lambda { i += 1 }
+        b << lambda { i -= 1 }
+        b << [lambda { i += 2}, lambda { i -= 1}]
+
+        i.should == 0
+        
+        sleep 0.1
+
+        i.should == 0
+
+        b.start
+        b.wait_until_done
+
+        i.should == 1
+      end
+
+      it "should support waiting with timeouts" do
+        i = 0
+        b = @T.new_batch
+        b << lambda { i += 1 }
+        b << lambda { i -= 1 }
+        b << [lambda { i += 2}, lambda { 500000000.times { i += 1}}]
+        t = Time.now
+        timeout = 0.2
+        b.wait_until_done(:timeout => timeout)
+
+        b.completed?.should be_false
+        (Time.now - t).should >= timeout
+        i.should > 2
+      end
+
+      it "should support 'completed?' even without timeouts" do
+        i = 0
+        b = @T.new_batch
+        b << lambda { i += 1 }
+        b << lambda { i -= 1 }
+        b << [lambda { i += 2}, lambda { sleep 0.01 while i < 10 }]
+
+        b.completed?.should be_false
+
+        sleep 0.1
+
+        b.completed?.should be_false
+
+        i = 10
+        sleep 0.1
+
+        b.completed?.should be_true
+      end
+
+      it "should support 'push'" do
+        i = 0
+        b = @T.new_batch
+        b.push(lambda { i += 1 })
+        b.push([lambda { i += 1 }, lambda { i += 1 }])
+        b.wait_until_done
+
+        i.should == 3
+      end
+
+      it "should support 'when_done'" do
+        i = 0
+        when_done_executed = false
+        b = @T.new_batch
+
+        # We're not testing what happens when 'when_done' is called and
+        # the batch is already finished, so wrapping in Thread#exclusive
+        Thread.exclusive do
+          100.times { b << lambda { i += 1 } }
+
+          b.completed?.should be_false
+        end
+
+        # Hmm, no guarantees that b hasn't completed by now, and can't wrap in
+        # Thread#exclusive because #when_done calls that too.
+        b.when_done { when_done_executed = true }
+
+        when_done_executed.should be_false
+
+        sleep(0.1)
+
+        b.completed?.should be_true
+        when_done_executed.should be_true
+      end
+
+      it "should call 'when_done' immediately when batch is already done" do
+        i = 0
+        when_done_executed = false
+        b = @T.new_batch
+
+        Thread.exclusive do
+          100.times { b << lambda { i += 1 } }
+
+          pending if b.completed?
+        end
+
+        b.wait_until_done
+
+        b.completed?.should be_true
+
+        b.when_done { when_done_executed = true }
+        
+        when_done_executed.should be_true
+      end
+
+      it "shouldn't fail" do
+        jobs = 1000
+        times_per_job = 100
+        i = 0
+
+        b1 = @T.new_batch(:latent => true)
+        b2 = @T.new_batch(:latent => true)
+        
+        jobs.times do
+          b1 << lambda { times_per_job.times { i += 1 } }
+          b2 << lambda { times_per_job.times { i -= 1 } }
+        end
+
+        b1.start
+        b2.start
+
+        b1.wait_until_done
+        b2.wait_until_done
+
+        i.should == 0
+      end
     end
   end
 end
